aea865bde8d480a86f9e8afc5959e7d46508d5aa
Broken_Diamond <liamw3382@hotmail.com>
2023-06-08 16:17:58 +0000

    Upload New File

---
+++ public/osana/osana.config.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";self.__osana$config={bare:`${location.origin}/bare/`,prefix:"/~osana/",codec:self.__osana$bundle.codecs.none,files:{config:"/osana.config.js",client:"/osana.client.js",bundle:"/osana.bundle.js",worker:"/osana.worker.js"},blacklist:[/^(www\.)?netflix\.com/,/^accounts\.google\.com/]}})();
+//# sourceMappingURL=osana.config.js.map
